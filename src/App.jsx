aa3c255f8b996237930a5d9b6312d5210fb58b00
Adriasu <adriana.suarez@students.gammatech.school>
2024-07-21 18:31:53 +0200

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import PokedexApi from './components/PokedexApi'
 
 const App = () => {
   return (
-    <div className="p-10 flex justify-center items-center bg-cover bg-center bg-no-repeat bg-[url('https://assets.pokemon.com//assets/cms2-es-es/img/misc/virtual-backgrounds/masters/forest.jpg')] ">
+    <div className="p-10 h-lvh flex justify-center items-center bg-cover bg-center bg-no-repeat bg-[url('https://assets.pokemon.com//assets/cms2-es-es/img/misc/virtual-backgrounds/masters/forest.jpg')] ">
         <PokedexApi/>
     </div>
   )
