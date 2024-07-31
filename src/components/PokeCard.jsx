f9017c74a6f6a5f8e97341d4040e4946a36c8186
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-07-31 11:38:13 +0200

    entrega de tarea

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -55,61 +55,64 @@ const PokeCard = ({ infoPokemon, funDelete }) => {
   const cssClassCards =
     "border-[10px] border-[#189e9a] rounded-2xl bg-opacity-80";
   return (
-    <Link to={`/${infoPokemon.id}`}>
-      <div className="inline-block w-[300px] text-left shadow-[0px_5px_20px_-10px_#111111] relative transition-[0.4s] m-2.5 p-[1em] rounded-2xl bg-[#ddd]">
-        <button
-          onClick={deletePokemon}
-          className="absolute top-3 right-3 size-8 rounded-full flex justify-center items-center bg-[#189e9a]"
-        >
-          <Trash2 />
-        </button>
-
-        <div className="bg-[rgba(0,0,0,0.7)] text-center pt-[1em] pb-0 px-[1em] rounded-[3px_3px_0_0]">
+    <div
+      className={`${cssClassCards} ${bgColorCard()} w-[360px] h-[620px] p-3 flex flex-col justify-center items-center gap-3 relative`}
+    >
+      <button
+        onClick={deletePokemon}
+        className="absolute top-3 right-3 size-8 rounded-full flex justify-center items-center bg-[#189e9a]"
+      >
+        <Trash2 />
+      </button>
+      <div className="w-[270px] h-[250px] rounded-full flex justify-center items-center border-[5px] border-[#189e9a] bg-gradient-to-r from-green-400 via-yellow-300 to-green-600">
+        <Link to={`/${infoPokemon.id}`}>
           <img
-            className="max-h-[150px] min-h-[150px] inline-block m-auto"
+            className="h-[250px] flex justify-center items-center"
             src={imgCard()}
             alt="https://www.google.com/search?sca_esv=254691dbe0298750&rlz=1C1UEAD_esES928ES928&sxsrf=ADLYWIJBT90q0QexW-Kwly4jueyPtjBIOA:1721735452556&q=pokemon+titulo+png&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgWizR0-0aFOH11Sb5tlNhd3zC4y7ZXTSrvvSBSNjw8fVX3G3tS3bGsqQeBBxb6Hy8QMktPbZ5tlXB9QufHYDIUU3RlFi1QS5IH2B-uHhf1Ar7zwncGzhjjXM7WwykVkpr2aYDQ44gNYxmUDe8MRVpUN63oVc&sa=X&ved=2ahUKEwjG0q-wjL2HAxV3UUEAHWa8AA4QtKgLegQIDhAB&biw=1536&bih=730&dpr=1.25#vhid=uTOm31sAW8oeJM&vssid=mosaic"
           />
+        </Link>
+      </div>
+      <div className="flex w-[300px] text-3xl justify-around font-bold">
+        <div className="uppercase border-[5px] border-[#189e9a] rounded-3xl p-1">
+          {infoPokemon.name}
         </div>
-
-        <div className="bg-[rgba(255,255,255,0.65)] relative p-[1em] rounded-[0_0_3px_3px]">
-          <div className=" flex capitalize text-center text-[1.5em] font-bold tracking-[0.02em] justify-around">
-            <div>{infoPokemon.name}</div>
-            <div>#{infoPokemon.id}</div>
-            <div>{infoPokemon.types[0].type.name}</div>
-          </div>
+        <div className="border-[5px] border-[#189e9a] rounded-3xl p-1">
+          #{infoPokemon.id}
         </div>
       </div>
-    </Link>
+      <div className="w-[360px] h-[226px] flex flex-col items-center">
+        {infoPokemon.stats.map((stats, i) => {
+          return <CardStats key={i} statsPokemon={stats} />;
+        })}
+      </div>
+    </div>
+
+    // ---------------------- Nuevo estilo ------------------------------------------------- //
+
     // <Link to={`/${infoPokemon.id}`}>
-    //   <div
-    //     className={`${cssClassCards} ${bgColorCard()} w-[360px] h-[620px] p-3 flex flex-col justify-center items-center gap-3 relative`}
-    //   >
+    //   <div className="inline-block w-[300px] text-left shadow-[0px_5px_20px_-10px_#111111] relative transition-[0.4s] m-2.5 p-[1em] rounded-2xl bg-[#ddd]">
     //     <button
     //       onClick={deletePokemon}
     //       className="absolute top-3 right-3 size-8 rounded-full flex justify-center items-center bg-[#189e9a]"
     //     >
     //       <Trash2 />
     //     </button>
-    //     <div className="w-[270px] h-[250px] rounded-full flex justify-center items-center border-[5px] border-[#189e9a] bg-gradient-to-r from-green-400 via-yellow-300 to-green-600">
+
+    //     <div className="bg-[rgba(0,0,0,0.7)] text-center pt-[1em] pb-0 px-[1em] rounded-[3px_3px_0_0]">
     //       <img
-    //         className="h-[250px] flex justify-center items-center"
+    //         className="max-h-[150px] min-h-[150px] inline-block m-auto"
     //         src={imgCard()}
     //         alt="https://www.google.com/search?sca_esv=254691dbe0298750&rlz=1C1UEAD_esES928ES928&sxsrf=ADLYWIJBT90q0QexW-Kwly4jueyPtjBIOA:1721735452556&q=pokemon+titulo+png&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgWizR0-0aFOH11Sb5tlNhd3zC4y7ZXTSrvvSBSNjw8fVX3G3tS3bGsqQeBBxb6Hy8QMktPbZ5tlXB9QufHYDIUU3RlFi1QS5IH2B-uHhf1Ar7zwncGzhjjXM7WwykVkpr2aYDQ44gNYxmUDe8MRVpUN63oVc&sa=X&ved=2ahUKEwjG0q-wjL2HAxV3UUEAHWa8AA4QtKgLegQIDhAB&biw=1536&bih=730&dpr=1.25#vhid=uTOm31sAW8oeJM&vssid=mosaic"
     //       />
     //     </div>
-    //     <div className="flex w-[300px] text-3xl justify-around font-bold">
-    //       <div className="uppercase border-[5px] border-[#189e9a] rounded-3xl p-1">
-    //         {infoPokemon.name}
+
+    //     <div className="bg-[rgba(255,255,255,0.65)] relative p-[1em] rounded-[0_0_3px_3px]">
+    //       <div className=" flex capitalize text-center text-[1.5em] font-bold tracking-[0.02em] justify-around">
+    //         <div>{infoPokemon.name}</div>
+    //         <div>#{infoPokemon.id}</div>
+    //         <div>{infoPokemon.types[0].type.name}</div>
     //       </div>
-    //       <div className="border-[5px] border-[#189e9a] rounded-3xl p-1">
-    //         #{infoPokemon.id}
-    //       </div>
-    //     </div>
-    //     <div className="w-[360px] h-[226px] flex flex-col items-center">
-    //       {infoPokemon.stats.map((stats, i) => {
-    //         return <CardStats key={i} statsPokemon={stats} />;
-    //       })}
     //     </div>
     //   </div>
     // </Link>
